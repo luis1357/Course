506b71c39728ef36395d3b824dc04d5a105daec3
Luis Enriquez <luis.enriquez1357@gmail.com>
2018-09-05 11:50:22 -0400

    week6day2

--- Reference/exoplayer-intro-master/Week6Day2/app/src/main/java/com/yeah/ruisu/week6day2/MainActivity.java
+++ Reference/exoplayer-intro-master/Week6Day2/app/src/main/java/com/yeah/ruisu/week6day2/MainActivity.java
@@ -4,14 +4,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    MyCustomView myCustomView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myCustomView = (MyCustomView) findViewById(R.id.myCstmVw);
     }
 
-    public void onClick(View view) {
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.btnOne:
+            {
+                myCustomView.customPaddingUp(30);
+                break;
+            }
+            case R.id.btnTwo:
+            {
+                myCustomView.swapColor();
+                break;
+            }
+            case R.id.btnThree:
+            {
+                myCustomView.customPaddingDown(30);
+                break;
+            }
+        }
     }
 }
